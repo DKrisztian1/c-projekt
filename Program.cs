c0fccdd02a46c5cdf3048a37482b930869e226b5
bekesi142 <72067150+bekesi142@users.noreply.github.com>
2023-01-13 11:45:33 +0100

    metodusok végleges

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        /// <summary>
+        /// Megadja, hogy hány termet tartamaz a térkép
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>Termek száma</returns>
+        static int GetRoomNumber(char[,] map)
+        {
+            int dbszoba = 0;
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    if (map[x,y] == '█')
+                    {
+                        dbszoba += 1;
+                    }
+                }
+            }
+            return dbszoba;
+        }
+        /// <summary>
+        /// A kapott térkép széleit végignézve megállapítja, hogy hány kijárat van.
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>Az alkalmas kijáratok száma</returns>
+        static int GetSuitableEntrance(char[,] map)
+        {
+            int dbkijarat = 0;
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    if (x == 0 && y ==0)
+                    {
+                        if (map[x, y] == '█')
+                        {
+                            dbkijarat += 1;
+                        }
+                        
+                    }
+                }
+            }
+            return dbkijarat;
+
+        }
+        /// <summary>
+        /// Megnézi, hogy van-e a térképen meg nem engedett karakter?
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>true - A térkép tartalmaz szabálytalan karaktert, false - nincs benne ilyen</returns>
+        static bool IsInvalidElement(char[,] map)
+        {
+            bool nemegengedett_karakter = false;
+            char[] megengedett_karakterek = { '╬', '═', '╦', '╩', '║', '╣', '╠', '╗', '╝', '╚', '╔', '█', '.' };
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    if (megengedett_karakterek.Contains(map[x,y]) == false)
+                    {
+                        nemegengedett_karakter = true;
+                    }
+                }
+            }
+            return nemegengedett_karakter; 
+        }
+        /// <summary>
+        /// Visszaadja azoknak a járatkaraktereknek a pozícióját, amelyekhez egyetlen szomszéd pozícióból sem lehet eljutni.
+        /// </summary>
+        /// <param name="map">Labirintus mátrixa</param>
+        /// <returns>A pozíciók "sor_index:oszlop_index" formátumban szerepelnek a lista elemeiként
+        static List<string> GetUnavailableElements(char[,] map)
+        {
+            List<string> unavailables = new List<string>();
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    
+                }
+            }
+            return unavailables;
+        }
+        /// <summary>
+        /// Labiritust generál a kapott pozíciókat tartalmazó lista alapján. A lista elemei egymáshoz kapcsolódó járatok pozíciói.
+        /// </summary>
+        /// <param name="positionsList">"sor_index:oszlop_index" formátumban az egymáshoz kapcsolódó járatok pozícióit tartalmazó lista </param>
+        /// <returns>A létrehozott labirintus térképe</returns>
+        static char[,] GenerateLabyrinth(List<string> positionsList)
+        {
+            return null;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
